9b649a856078881b3be9a45f3b537ca03d82c126
Daniel Abraham Enriqeuz Colqui <elcolorloestodo@gmail.com>
2022-02-19 23:52:58 -0500

    Actualización del header.

--- personal_page/index-dom.js
+++ personal_page/index-dom.js
@@ -1,6 +1,15 @@
 import hamburgerMenu from "./menu-hamburguesa.js";
-
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
     hamburgerMenu(".panel-btn" , ".header-nav__container-list-desactibe" , ".header-nav-link__items");
 });
+
+window.onscroll = function () {
+    let scroll = document.documentElement.scrollTop;
+    let header = document.getElementById("header");
+    if (scroll > 64) {
+        header.classList.add('header-md');
+    } else if (scroll < 64) {
+        header.classList.remove('header-md');
+    };
+};
